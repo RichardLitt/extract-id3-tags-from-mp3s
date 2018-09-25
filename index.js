2faf5b352edac1a71fd062d342765fc345b2550b
Richard Littauer <richard+github@burntfen.com>
2018-09-25 10:20:47 -0400

    Add a small change

--- index.js
+++ index.js
@@ -3,5 +3,9 @@ var mm = require('musicmetadata');
 
 var parser = mm(fs.createReadStream('src/Venice\ 182-.mp3'), function (err, metadata) {
   if (err) throw err;
+  mm(fs.createReadStream('src/Venice\ 182-.mp3'), { duration: true }, function (err, metadata) {
+    if (err) throw err
+    console.log(metadata)
+  });
   console.log(metadata);
 });
